7a1dae8ee8cbedf3ac1717ce616291389cbefa1e
MasamiOnoue <masami.onoue@outlook.com>
2025-06-05 14:09:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -294,10 +294,11 @@ threading.Thread(target=summarize_and_store_daily_logs, daemon=True).start()
 
 def load_summary_memory(days=7):
     try:
-        rows = sheet.values().get(
-            spreadsheetId=SPREADSHEET_ID5,
-            range='経験ログ!A2:B'
-        ).execute().get("values", [])[1:]
+        #一時的にコメントアウト
+        #rows = sheet.values().get(
+        #    spreadsheetId=SPREADSHEET_ID5,
+        #    range='経験ログ!A2:B'
+        #).execute().get("values", [])[1:]
 
         today = datetime.datetime.now(JST).date()
         summaries = []
