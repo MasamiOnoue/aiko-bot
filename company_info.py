3a46e36f6cec5c91ef34fcf5f3d1bc96a1c1bf2c
MasamiOnoue <masami.onoue@outlook.com>
2025-06-09 23:57:01 +0900

    Update company_info.py

--- company_info.py
+++ company_info.py
@@ -24,6 +24,8 @@ def search_employee_info_by_keywords(user_message, employee_info_list):
         "まさみ": "政美",
         "かおり": "香織",
         "こうちゃん": "孝一",
+        "考ちゃん": "孝一",
+        "工場長": "折戸",
         # 必要に応じて追加
     }
     attributes = {
