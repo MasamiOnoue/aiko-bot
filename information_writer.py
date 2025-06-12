666e506eb41b7d7b7f4154f8a01b1971c86a4f55
MasamiOnoue <masami.onoue@outlook.com>
2025-06-12 19:33:34 +0900

    Update information_writer.py

--- information_writer.py
+++ information_writer.py
@@ -52,22 +52,3 @@ def send_employee_info(values):
         logging.info("✅ 従業員情報送信成功")
     except Exception as e:
         logging.error(f"❌ Cloud Function呼び出し失敗: {e}")
-
-def get_employee_info():
-    try:
-        base_url = os.getenv("GCF_ENDPOINT")
-        if not base_url:
-            raise ValueError("GCF_ENDPOINT 環境変数が設定されていません")
-
-        url = base_url.rstrip("/") + "/read-employee-info"
-        api_key = os.getenv("PRIVATE_API_KEY")
-        headers = {
-            "x-api-key": api_key
-        }
-        response = requests.get(url, headers=headers, timeout=10)
-        response.raise_for_status()
-        logging.info("✅ 従業員情報取得成功")
-        return response.json().get("data", [])
-    except Exception as e:
-        logging.error(f"❌ Cloud Function呼び出し失敗: {e}")
-        return []
