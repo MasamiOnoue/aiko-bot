0440dd08d98e1bf4ef73d6fa525593103e65e9e2
MasamiOnoue <masami.onoue@outlook.com>
2025-06-12 22:40:54 +0900

    Update aiko_diary_report.py

--- aiko_diary_report.py
+++ aiko_diary_report.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 import pytz
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-from sheet_service import get_google_sheets_service
+from sheets_service import get_google_sheets_service
 #from company_info_load import get_conversation_log, get_google_sheets_service
 #from company_info_save import write_company_info
 from openai_client import client  # OpenAIクライアント
