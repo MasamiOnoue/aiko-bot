a50a29e32d4f041e1561d4646d88616a6e995ec0
MasamiOnoue <masami.onoue@outlook.com>
2025-06-14 17:25:50 +0900

    Update handle_message_logic.py

--- handle_message_logic.py
+++ handle_message_logic.py
@@ -223,7 +223,7 @@ def handle_message_logic(event, sheet_service, line_bot_api):
         "従業員情報": search_employee_info_by_keywords(user_message, employee_info),
         "取引先情報": search_partner_info_by_keywords(user_message, get_partner_info()),
         "会社情報": search_company_info_log(user_message, get_company_info()),
-        "経験ログ": search_experience_log(user_message, get_experience_log()),
+        "経験ログ": search_aiko_experience_log(user_message, get_aiko_experience_log()),
         "会話ログ": search_conversation_log(user_message, get_conversation_log())
     }
     log_if_all_searches_failed(results)
@@ -233,7 +233,7 @@ def handle_message_logic(event, sheet_service, line_bot_api):
         try:
             if contains_sensitive_info(user_message):
                 combined = []
-                for dataset in [employee_info, get_partner_info(), get_company_info(), get_conversation_log(), get_experience_log()]:
+                for dataset in [employee_info, get_partner_info(), get_company_info(), get_conversation_log(), get_aiko_experience_log()]:
                     combined.extend([str(item) for item in dataset if any(w in str(item) for w in user_message.split())])
                 hits = combined[:MAX_HITS] or ["該当情報が見つかりませんでした。"]
                 masked_input, mask_map = mask_sensitive_data("\n".join(hits))
